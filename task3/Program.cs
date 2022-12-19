8243a5198fe57e1a0f7f259473329406e3d19c08
Nataliia <nataliyatrishechkina@mail.ru>
2022-12-20 00:30:10 +0300

    final version

---
+++ task3/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 18 18
+
+int[,] firstMartrices = new int[2, 2];
+GetArray(firstMartrices);
+Console.WriteLine("Матрица А\t ");
+PrintArray(firstMartrices);
+
+int[,] secondMartrices = new int[2, 2];
+GetArray(secondMartrices);
+Console.WriteLine("Матрица В\t ");
+PrintArray(secondMartrices);
+
+int[,] resultMatrices = new int[2, 2];
+
+GetProductTwoMatrices(firstMartrices, secondMartrices, resultMatrices);
+Console.WriteLine("Произведение двух матриц:\t ");
+PrintArray(resultMatrices);
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetProductTwoMatrices(int[,] firstMartrix, int[,] secondMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secondMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
